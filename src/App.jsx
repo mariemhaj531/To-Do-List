d8d3f4aaf9ae32b77c18f65e80a898e7106a4ddf
mariemhaj531 <mariemhajji28531@gmail.com>
2025-02-18 16:45:03 +0100

    Store App Data in LocalStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Header from './components/Header/Header'
 import TodoList from './components/TodoList/TodoList'
 import AddTodo from './components/AddTodo/AddTodo'
@@ -6,7 +6,23 @@ import './styles/styles.css'
 // import './index.css'
 const App = () => {
   const [todos, setTodos] = useState([]);
-
+  const [editing, setEditing] = useState(null);
+  // LOAD  TODOS FROM LOCALSTORAGE
+  useEffect(() => {
+    const storedTodos = localStorage.getItem("todos");
+    if (storedTodos) {
+      try {
+      setTodos(JSON.parse(storedTodos));
+    } catch (error) {
+      console.error("Error parsing JSON from localStorage", error);
+      setTodos([]);
+    }
+    }
+  }, []);
+  //Save todos to localstorage
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
   const addTodo = (title) => {
     const newTodo = { id: Date.now(), title, completed: false };
     setTodos([...todos, newTodo]);
@@ -19,13 +35,28 @@ const App = () => {
   const deleteTodo = (id) => {
     setTodos(todos.filter(todo => todo.id !== id));
   };
+  const editTodo = (id, newTitle) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, title: newTitle } : todo
+      )
+    );
+    setEditing(null);
+  };
   return (
     <div className='app'>
       <Header />
       <AddTodo addTodo={addTodo} />
-      <TodoList todos={todos} toggleComplete={toggleComplete} deleteTodo={deleteTodo} />
+      <TodoList
+        todos={todos}
+        toggleComplete={toggleComplete}
+        deleteTodo={deleteTodo}
+        editTodo={editTodo}
+        setEditing={setEditing}
+        editing={editing}
+      />
     </div>
   );
-}
+};
 
 export default App
